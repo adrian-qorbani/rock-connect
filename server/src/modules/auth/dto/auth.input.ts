94cf9379c7cffdf448879b1c7d33f846a2f0df0a
mahdi-developer <mahdi.ghorbani96io@gmail.com>
2025-03-21 09:04:09 +0330

    fix (auth): added missing input validators to auth dto.

--- server/src/modules/auth/dto/auth.input.ts
+++ server/src/modules/auth/dto/auth.input.ts
@@ -1,10 +1,17 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsString, MaxLength, MinLength } from 'class-validator';
 
    @InputType()
    export class AuthInput {
+     @IsString()
+     @MinLength(4)
+     @MaxLength(20)
      @Field()
      username: string;
 
+     @IsString()
+     @MinLength(4)
+     @MaxLength(20)
      @Field()
      password: string;
    }
