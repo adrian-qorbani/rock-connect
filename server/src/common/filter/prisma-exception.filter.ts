327dbacf0f291282cf6755bf49757175c74b3112
mahdi-developer <mahdi.ghorbani96io@gmail.com>
2025-03-21 09:04:40 +0330

    chore: added validator exception handling to filters.

--- server/src/common/filter/prisma-exception.filter.ts
+++ server/src/common/filter/prisma-exception.filter.ts
@@ -4,13 +4,25 @@ import {
   ConflictException,
   NotFoundException,
   UnprocessableEntityException,
+  BadRequestException,
 } from '@nestjs/common';
 import { GqlExceptionFilter } from '@nestjs/graphql';
 import { Prisma } from '@prisma/client';
+import { ValidationError } from 'class-validator';
 
-@Catch(Prisma.PrismaClientKnownRequestError)
+@Catch(Prisma.PrismaClientKnownRequestError, ValidationError)
 export class PrismaClientExceptionFilter implements GqlExceptionFilter {
-  catch(exception: Prisma.PrismaClientKnownRequestError): any {
+  catch(
+    exception: Prisma.PrismaClientKnownRequestError | ValidationError,
+  ): any {
+    if (exception instanceof Prisma.PrismaClientKnownRequestError) {
+      return this.handlePrismaError(exception);
+    } else if (exception instanceof ValidationError) {
+      return this.handleValidationError(exception);
+    }
+  }
+
+  private handlePrismaError(exception: Prisma.PrismaClientKnownRequestError) {
     logger.error(
       `Prisma Error Code: ${exception.code}, Message: ${exception.message}`,
     );
@@ -29,8 +41,28 @@ export class PrismaClientExceptionFilter implements GqlExceptionFilter {
         throw new NotFoundException(`${model} not found.`);
       }
       default:
-        break;
+        throw new BadRequestException('An unexpected database error occurred.');
     }
-    return exception;
+  }
+
+  private handleValidationError(exception: ValidationError) {
+    logger.error(`Validation Error: ${exception.toString()}`);
+
+    const errors = this.flattenValidationErrors(exception);
+    throw new BadRequestException({
+      statusCode: 400,
+      message: 'Validation failed',
+      errors,
+    });
+  }
+
+  private flattenValidationErrors(error: ValidationError): string[] {
+    const errors: string[] = []; 
+    for (const constraint in error.constraints) {
+      if (error.constraints.hasOwnProperty(constraint)) {
+        errors.push(error.constraints[constraint]); 
+      }
+    }
+    return errors;
   }
 }
