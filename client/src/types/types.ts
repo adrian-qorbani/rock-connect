5347d1855efe7d8a002135abac14091106637000
mahdi-developer <mahdi.ghorbani96io@gmail.com>
2025-03-23 08:46:57 +0330

    feat (types): added types for user and post.

---
+++ client/src/types/types.ts
@@ -0,0 +1,12 @@
+export interface User {
+    id: string;
+    name: string;
+  }
+  
+  export interface Post {
+    id: string;
+    title: string;
+    author: string;
+    publishedAt: string;
+    content: string;
+  }
